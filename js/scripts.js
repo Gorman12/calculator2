b27fc7551d4a46b3f4f15152107218bb1d2cc92b
Michael Poehner and sean gorman <michael@>
2016-09-14 20:14:34 -0700

    Show that there are 4 collumns with 4 forms where the user can enter values and the website will display the computed output

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
   return number1 + number2;
 };
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// User interface (or front-end) logic:
+
 
 $(document).ready(function() {
   $("form#add").submit(function(event) {
@@ -8,6 +25,31 @@ $(document).ready(function() {
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text("This is your result: " + result)
+    $("#output-add").text("This is your result: " + result)
   });
+
+  $("form#subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#subtract1").val());
+    var number2 = parseInt($("#subtract2").val());
+    var result = subtract(number1, number2);
+    $("#output-subtract").text("This is your result: " + result)
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2);
+    $("#output-multiply").text("This is your result: " + result)
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#divide1").val());
+    var number2 = parseInt($("#divide2").val());
+    var result = divide(number1, number2);
+    $("#output-divide").text("This is your result: " + result)
+  });
+
 });
